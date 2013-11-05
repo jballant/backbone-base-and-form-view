e99e4e5d31cc46891d4a49f9617b0f47d01028d2
James Ballantine <james@1stdibs.com>
2013-11-05 12:58:46 -0500

    Added files and config to add and run jasmine tests

---
+++ tests/spec/backbone-baseview_Spec.js
@@ -0,0 +1,7 @@
+/*global describe, it, expect, spyOn, waitsFor, beforeEach */
+describe('Backbone.BaseView', function () {
+    "use strict";
+    it('should ensure that true is true', function () {
+        expect(true).toBe(true);
+    });
+});
